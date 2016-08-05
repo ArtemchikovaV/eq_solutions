8da65608d011eed40c2607e237f5d01d1f8435a3
MorozenkoVitaminka <Morozenko_viola@mail.ru>
2016-08-05 23:51:42 +0300

    Add files via upload

---
+++ eq_solution.cpp
@@ -0,0 +1,117 @@
+//equation solutions
+//D > 0 - two roots x1, x2
+//D == 0 - two same roots x1 = x2
+//D < 0 - there is no root -INF, -INF
+// INF, INF - infinity number of roots
+// x, -INF - one root
+
+#include <iostream>
+#include <limits>
+#include <math.h>
+#include <float.h>
+
+
+struct Pair
+{
+	double first;
+	double second;
+};
+
+Pair* eq_solution( double a, double b, double c )
+{
+	double inf = std::numeric_limits<double>::infinity();
+	
+	double D = 0.0;						//discriminant
+	double eps = 0.000001;				//epsilon. if mod(number) < epsilon then number == 0 
+	double x1 = 0.0, x2 = 0.0;			//solutions
+	Pair root;
+	//root->first = 0;
+	//root->second = 0;
+
+	if ( a == 0 && b == 0 && c == 0 )
+	{
+		root.first = inf;
+		root.second = inf;
+		return &root;
+	}
+
+	if ( a == 0 && b == 0 && c != 0 )
+	{
+		root.first = -inf;
+		root.second = -inf;
+		return &root;
+	}
+
+	if ( ( a != 0 && b == 0 && c == 0 ) )
+	{
+		root.first = 0.0;
+		root.second = 0.0;
+		return &root;
+	}
+
+		if (( a == 0 && b != 0 && c == 0 ))
+	{
+		root.first = 0.0;
+		root.second = -inf;
+		return &root;
+	}
+
+	if (a != 0)
+	{
+		D = b*b - 4*a*c;
+		if ( D > 0)
+		{
+			root.first = (-b + sqrt(D))/2/a;
+	 		root.second = (-b - sqrt(D))/2/a;
+		}
+		else
+		{
+			if( abs(D) < eps )
+				root.first = root.second = -b/2/a;
+			else 
+				root.first = root.second = -inf;
+
+		}
+	}
+	else
+	{	
+		root.first = -c/b;
+		root.second = -inf;
+	}
+
+return &root;
+}
+
+void test( );
+
+int main()
+{				
+	test();
+	return 0;
+}
+
+void test( )
+{
+	Pair *root;
+	printf("Coefficients a, b, c: 0.0, 0.0, 0.0\n" );
+	root = eq_solution(0.0, 0.0, 0.0);
+	printf("Roots: %f, %f\n",root->first, root->second);
+	printf("Coefficients a, b, c: 0.0, 0.0, 1.0\n" );
+	root = eq_solution(0.0, 0.0, 1.0);
+	printf("Roots: %f, %f\n",root->first, root->second);
+	printf("Coefficients a, b, c: 2.0, 0.0, 0.0\n" );
+	root = eq_solution(2.0, 0.0, 0.0);
+	printf("Roots: %f, %f\n",root->first, root->second);
+	printf("Coefficients a, b, c: 0.0, 4.0, 0.0\n" );
+	root = eq_solution(0.0, 4.0, 0.0);
+	printf("Roots: %f, %f\n",root->first, root->second);
+	printf("Coefficients a, b, c: 1.0, 2.0, -3.0. D > 0\n" );
+	root = eq_solution(1.0, 2.0, -3.0);
+	printf("Roots: %f, %f\n",root->first, root->second);
+	printf("Coefficients a, b, c: 1.0, 2.0, 1.0. D = 0\n" );
+	root = eq_solution(1.0, 2.0, 1.0);
+	printf("Roots: %f, %f\n",root->first, root->second);
+	printf("Coefficients a, b, c: 2.0, 3.0, 4.0. D < 0\n" );
+	root = eq_solution(2.0, 3.0, 4.0); 
+	printf("Roots: %f, %f\n",root->first, root->second);	
+}
